4c19e7b1ebdeaec16ed32aa9fcebfe31ce53c43b
Wayne He <epicwaynehe@gmail.com>
2020-12-06 10:00:18 -0500

    optimized first part code

--- day6/first.py
+++ day6/first.py
@@ -3,10 +3,7 @@ questions_sum = 0
 
 for group in groups:
     raw_answers = group.replace("\n", "")
-    unique_answers = set()
-    for letter in raw_answers:
-        unique_answers.add(letter)
-
+    unique_answers = set(raw_answers)
     questions_sum += len(unique_answers)
 
 print(questions_sum)
