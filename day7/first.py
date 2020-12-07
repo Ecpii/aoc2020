881b99d888bb67e85ec9ee3727b5d6ea336df959
Wayne He <epicwaynehe@gmail.com>
2020-12-07 04:53:52 -0500

    day7 first part, result 316

---
+++ day7/first.py
@@ -0,0 +1,15 @@
+rules = open("input.txt").readlines()
+
+
+def find_exterior_bags(bag):
+    outside_bags = set()
+    for rule in rules:
+        if bag in rule and rule[:rule.index("bag") - 1] != bag:
+            print(rule[:rule.index("bag") - 1])
+            outside_bags.add(rule[:rule.index("bag") - 1])
+            outside_bags = outside_bags.union(find_exterior_bags(rule[:rule.index("bag") - 1]))
+    print(f"{outside_bags = }")
+    return outside_bags
+
+
+print(len(find_exterior_bags("shiny gold")))
