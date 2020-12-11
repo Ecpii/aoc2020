ddf8ee6cdea1e620e7d522b7b3c560cc8724109f
Wayne He <epicwaynehe@gmail.com>
2020-12-11 11:28:47 -0500

    made first part code shorter

--- day11/first.py
+++ day11/first.py
@@ -2,24 +2,11 @@ with open("input.txt") as inp:
     seats = inp.read().split("\n")
 
 seats.pop()
-prev_seats = seats.copy()
 changes = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (0, -1), (1, -1), (1, 0), (1, 1)]
 end_num_occupied = 0
 
 
-def check_empty(row_index, col_index):
-    for row_change, col_change in changes:
-        try:
-            if row_index + row_change < 0 or col_index + col_change < 0:
-                continue
-            if prev_seats[row_index + row_change][col_index + col_change] == "#":
-                return False
-        except IndexError:
-            continue
-    return True
-
-
-def check_occupied(row_index, col_index):
+def check_empty(row_index, col_index, occupied):
     num_occupied = 0
     for row_change, col_change in changes:
         try:
@@ -27,11 +14,11 @@ def check_occupied(row_index, col_index):
                 continue
             if prev_seats[row_index + row_change][col_index + col_change] == "#":
                 num_occupied += 1
-            if num_occupied == 4:
-                return True
+            if num_occupied == (4 if occupied else 1):
+                return False
         except IndexError:
             continue
-    return False
+    return True
 
 
 while True:
@@ -39,17 +26,16 @@ while True:
     for i in range(len(seats) * len(seats[0])):
         row = i // len(seats[0])
         col = i % len(seats[0])
-        if seats[row][col] == 'L' and check_empty(row, col):
+        # this code is slow but i can't be bothered to change it
+        if seats[row][col] == 'L' and check_empty(row, col, False):
             seats[row] = seats[row][:col] + '#' + seats[row][col + 1:]
-        elif seats[row][col] == '#' and check_occupied(row, col):
+        elif seats[row][col] == '#' and not check_empty(row, col, True):
             seats[row] = seats[row][:col] + 'L' + seats[row][col + 1:]
     if prev_seats == seats:
         break
 
 for seat_row in seats:
-    for char in seat_row:
-        if char == '#':
-            end_num_occupied += 1
+    end_num_occupied += seat_row.count("#")
 
 print(end_num_occupied)
 # >>> 2211
