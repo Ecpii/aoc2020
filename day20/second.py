3167a090311edc5b52fc3b09a579aa8d85a993e6
Wayne He <epicwaynehe@gmail.com>
2020-12-23 07:52:59 -0500

    documented and refactored code

--- day20/second.py
+++ day20/second.py
@@ -4,22 +4,22 @@ tile_length = 10
 image_tile_length = int(len(tile_dict) ** .5)
 image_length = (tile_length - 2) * image_tile_length
 
-tile_orientation = [
-    [] for i in range(image_tile_length)
-]
-
 
 def rotate(tile_id: int, degrees: int) -> None:
+    """Rotates a given tile counterclockwise in tile_dict given id and degrees (multiple of 90)"""
     if degrees == 0:
         return
     for j in range(degrees // 90):
         tile = tile_dict[tile_id]
         new_tile = ['' for _ in range(len(tile))]
         for row in tile:
+            # builds each column from bottom to top, right to left of new tile
+            # with each row from left to right, top to bottom
             for i in range(len(tile)):
                 new_tile[len(tile) - 1 - i] += row[i]
         tile_dict[tile_id] = new_tile
     if tile_id != -1:
+        # recalculate the edges of the rotated tile
         new_edge_matches = [None for _ in range(4)]
         for i in range(4):
             new_edge_matches[(i + degrees // 90) % 4] = edge_matches[tile_id][i]
@@ -28,6 +28,7 @@ def rotate(tile_id: int, degrees: int) -> None:
 
 
 def flip_tile(tile_id: int, horizontal: bool) -> None:
+    """Flips a tile with a given id and boolean to specify a horizontal or vertical flip"""
     if horizontal:
         for i in range(len(tile_dict[tile_id])):
             tile_dict[tile_id][i] = tile_dict[tile_id][i][::-1]
@@ -37,7 +38,7 @@ def flip_tile(tile_id: int, horizontal: bool) -> None:
         indexes = (0, 2)
 
     if tile_id != -1:
-        # switch the edges on the ends that are parallel to the axis of rotation/flipping
+        # recalculate the edges of the flipped tile
         edge_matches[tile_id][indexes[0]], edge_matches[tile_id][indexes[1]] = \
             edge_matches[tile_id][indexes[1]], edge_matches[tile_id][indexes[0]]
         tile_edges[tile_id][indexes[0]], tile_edges[tile_id][indexes[1]], = \
@@ -45,7 +46,9 @@ def flip_tile(tile_id: int, horizontal: bool) -> None:
         find_edges(tile_id)
 
 
+# now unused function that was used for debugging and seeing each tile
 def pretty_print(tile_id: int) -> None:
+    """Prints a tile with coloring and information about its edges"""
     print(f"\nTile {tile_id}:")
     whole_tile = tile_dict[tile_id]
     for line in whole_tile:
@@ -60,7 +63,8 @@ def pretty_print(tile_id: int) -> None:
 
 
 def pretty_print_image() -> None:
-    print("\nAligned Image:")
+    """Prints the final image with coloring as well as information about the water roughness"""
+    print("\nFinal Image:")
     water_roughness = 0
     whole_image = tile_dict[-1]
     for line in whole_image:
@@ -77,19 +81,25 @@ def pretty_print_image() -> None:
 
 
 def build_row(row_num: int) -> None:
+    """Fills a row of the image with tile_ids, given that the row already has a tile_id
+    in the beginning."""
     previous_tile_id = tile_orientation[row_num][-1]
     while edge_matches[previous_tile_id][3]:
         current_tile_id = edge_matches[previous_tile_id][3]
+        # rotates the tile such that the matching sides are connected
         index_difference = 5 - edge_matches[current_tile_id].index(previous_tile_id)
-
         rotate(current_tile_id, index_difference * 90 % 360)
+
+        # if they're connected in the wrong way, flip it vertically
         if tile_edges[current_tile_id][1] != tile_edges[previous_tile_id][3]:
             flip_tile(current_tile_id, False)
+
         tile_orientation[row_num].append(current_tile_id)
         previous_tile_id = tile_orientation[row_num][-1]
 
 
 def orient_first_tile(row_num: int) -> None:
+    """Rotates the first tile in a row to match the left edge and the tile above."""
     previous_row_start_id = tile_orientation[row_num - 1][0]
     first_tile_id = edge_matches[previous_row_start_id][2]
     index_difference = 4 - edge_matches[first_tile_id].index(previous_row_start_id)
@@ -100,7 +110,8 @@ def orient_first_tile(row_num: int) -> None:
     tile_orientation[row_num].append(first_tile_id)
 
 
-def build_image(orientation: list) -> list:
+def assemble_image(orientation: list) -> list:
+    """Uses an array of tile ids to create a string array representing the image."""
     final_image = ['' for _ in range(image_length)]
     for tile_row_num in range(image_tile_length):
         for tile_id in orientation[tile_row_num]:
@@ -111,16 +122,24 @@ def build_image(orientation: list) -> list:
     return final_image
 
 
-def find_loch_ness(start_y: int, start_x: int) -> set:
-    coordinates = set()
+def find_loch_ness(start_y: int, start_x: int) -> list:
+    """Attempts to find a loch ness monster with the given starting coords
+    (looks in the fourth quadrant relative to the starting coordinates)"""
+    coordinates = list()
     for offset in loch_ness_pattern:
         if tile_dict[-1][start_y + offset[0]][start_x + offset[1]] in {'#', 'O'}:
-            coordinates.add((start_y + offset[0], start_x + offset[1]))
+            coordinates.append((start_y + offset[0], start_x + offset[1]))
         else:
-            return set()
+            return []
     return coordinates
 
 
+# an array to store the positions of each tile id
+tile_orientation = [
+    [] for i in range(image_tile_length)
+]
+
+# generates the first corner and rotates it correctly
 first_corner_id = 0
 for edge_tile_id in edge_matches:
     if edge_matches[edge_tile_id].count(None) == 2:
@@ -133,23 +152,13 @@ while edge_matches[first_corner_id][0] or edge_matches[first_corner_id][1]:
 tile_orientation[0].append(first_corner_id)
 build_row(0)
 
+# builds each row and assembles the image at the end, assigns it to id -1
 for i in range(1, image_tile_length):
     orient_first_tile(i)
     build_row(i)
+tile_dict[-1] = assemble_image(tile_orientation)
 
-tile_dict[-1] = build_image(tile_orientation)
-
-image_orientation_instructions = [
-    (0, False, False),
-    (90, False, False),
-    (180, False, False),
-    (270, False, False),
-    (0, True, True),
-    (0, True, False),
-    (90, True, False),
-    (90, True, True)
-]
-
+# generates the pattern from the loch ness string
 loch_ness = """
                   # 
 #    ##    ##    ###
@@ -160,24 +169,16 @@ for y in range(len(loch_ness)):
     for x in range(len(loch_ness[y])):
         if loch_ness[y][x] == '#':
             loch_ness_pattern.append((y, x))
-loch_ness_coordinates = set()
 
-for instruction in image_orientation_instructions:
-    rotate(-1, instruction[0])
-    if instruction[1]:
-        flip_tile(-1, instruction[2])
-
-    for y in range(len(tile_dict[-1]) - len(loch_ness)):
-        for x in range(len(tile_dict[-1][0]) - len(loch_ness[0]) - 1):
-            possible_loch_ness_coords = find_loch_ness(y, x)
-            if possible_loch_ness_coords:
-                for coord in possible_loch_ness_coords:
-                    tile_dict[-1][coord[0]] = tile_dict[-1][coord[0]][:coord[1]] + 'O' + \
-                                              tile_dict[-1][coord[0]][coord[1] + 1:]
-
-    rotate(-1, 360 - instruction[0])
-    if instruction[1]:
-        flip_tile(-1, instruction[2])
+# luckily, my pattern didn't need to be rotated or flipped, so code there is removed
+# (but it's in a previous commit)
+for y in range(len(tile_dict[-1]) - len(loch_ness)):
+    for x in range(len(tile_dict[-1][0]) - len(loch_ness[0]) - 1):
+        possible_loch_ness_coords = find_loch_ness(y, x)
+        if possible_loch_ness_coords:
+            for coord in possible_loch_ness_coords:
+                tile_dict[-1][coord[0]] = tile_dict[-1][coord[0]][:coord[1]] + 'O' + \
+                                          tile_dict[-1][coord[0]][coord[1] + 1:]
 
 pretty_print_image()
 # >>> 1649
